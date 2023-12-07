4bd48df033e8a96e7507af1c0af7a89e0c6a261c
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-07 18:03:21 -0500

    basic navbar styling

--- svelte-app/src/Navbar.svelte
+++ svelte-app/src/Navbar.svelte
@@ -14,3 +14,40 @@
         </ul>
     </div>
 </nav>
+
+
+<style>
+    nav {
+        background-color: #333; /* Dark background */
+        color: white;
+        padding: 10px 0;
+    }
+
+    nav div {
+        max-width: 1200px; /* Maximum width */
+        margin: 0 auto;
+        padding: 0 20px;
+    }
+
+    ul {
+        list-style: none; /* Remove default list styling */
+        padding: 0;
+        margin: 0;
+        display: flex; /* Align items in a row */
+        justify-content: space-around; /* Even space between the items */
+    }
+
+    li {
+        margin: 0 10px; /* Spacing between list items */
+    }
+
+    a {
+        color: white; /* White text color for links */
+        text-decoration: none; /* Remove underline from links */
+        font-weight: bold;
+    }
+
+    a:hover {
+        color: #ddd; /* Light grey on hover */
+    }
+</style>
