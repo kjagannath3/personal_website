59e14956e895a8df353a4d26d3b1d9f495f09783
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-12 03:58:56 -0500

    fixed the router issue

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -2,7 +2,7 @@
 	export let name;
 	import Navbar from "./Navbar.svelte";``
 	import Router from 'svelte-spa-router';
-	//import { routes } from './routes';
+	import { routes } from './routes';
 
 </script>
 
--- svelte-app/src/routes.js
+++ svelte-app/src/routes.js
@@ -1,10 +1,8 @@
 import Education from './Education.svelte';
 import Music from './Music.svelte';
 import Projects from './Projects.svelte';
-import App from './App.svelte';
 
 export const routes = [
-    {'/': App},
     {'/Education': Education},
     {'/Music': Music},
     {'/Projects': Projects}
