30ee43f5cfe84c609b37b6715b7a42048e6692f2
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-14 02:46:21 -0500

    created new music page

---
+++ svelte-app/src/Music.svelte
@@ -0,0 +1,26 @@
+<script>
+
+</script>
+
+
+
+<music>
+    <div class="text-box">
+        <p>lorem ipsur dejbf kadsbjfbhbhbhbafkdhsbfjhb hdfbakjdhsbfidbfhsadbfkjasdhbfuywae hwebf jasbd kjhbafudsbfubadskufyb</p>
+    </div>
+</music>
+
+
+<style>
+    .text-box {
+        width: 100%;
+        height: 100%;
+        background-color: #000;
+        color: #fff;
+        text-align: center;
+    }
+</style>
+
+
+
+
