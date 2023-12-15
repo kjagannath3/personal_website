295df32635d8dc7bb0ba1aa3e540ac4dee919cac
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-15 00:46:02 -0500

    hover color fixed

--- svelte-app/src/Navbar.svelte
+++ svelte-app/src/Navbar.svelte
@@ -30,21 +30,6 @@
         display: flex; /* Align buttons in a row */
         justify-content: space-around; /* Even space between buttons */
     }
-    Link { /* Style for <a> or <Link> tags */
-    color: white; /* White text color */
-    text-decoration: none; /* Remove underline from links */
-    padding: 10px 20px;
-}
-    button {
-        background: none; /* Remove default button background */
-        border: none; /* Remove default button border */
-        color: white; /* White text color */
-        font-weight: bold;
-        cursor: pointer;
-        padding: 10px 20px;
-    }
 
-    button:hover {
-        background-color: rgba(255, 255, 255, 0.2); /* Light background on hover */
-    }
+
 </style>
