4a19fbc5505d2d4eef62beb2246c886aabaae0a8
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-16 15:24:10 +0530

    started adding before checking out

--- svelte-app/src/Home.svelte
+++ svelte-app/src/Home.svelte
@@ -6,6 +6,19 @@
 <main>
 	<h1 class="big-title">Hello World!</h1>
 	<p class="big-paragraph">Welcome to the world of Keshav Jagannath!</p>
+	<div class ="text-picture-container">
+		<h2>Lorem Ipsum</h2>
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non urna at libero sodales blandit. 
+			Suspendisse sed mi eu sem blandit tristique. Sed eleifend nisl non lacus vulputate, eget posuere massa 
+			hendrerit. Proin auctor, elit eget consectetur tristique, dolor risus malesuada nisi, ac interdum quam 
+			libero a felis. Sed a tincidunt arcu. Nullam tincidunt semper orci, at lobortis dui congue nec. Curabitur 
+			tincidunt, nisi nec rhoncus feugiat, erat lorem iaculis metus, id venenatis nunc lectus sed nisl. Quisque 
+			ut euismod nulla. Sed vel lectus ipsum. Vivamus at nunc nec nisi eleifend congue. Vivamus convallis sapien 
+			ut dapibus tincidunt. Vivamus sit amet ipsum eget justo facilisis venenatis. Nulla facilisi.
+		</p>
+		
+	</div>
 </main>
 
 
