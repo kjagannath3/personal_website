c6c628470de1f0d902f9efc6141d54ef4d1f7fed
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-16 15:48:10 +0530

    Created container hierarchy

--- svelte-app/src/Home.svelte
+++ svelte-app/src/Home.svelte
@@ -7,17 +7,21 @@
 	<h1 class="big-title">Hello World!</h1>
 	<p class="big-paragraph">Welcome to the world of Keshav Jagannath!</p>
 	<div class ="text-picture-container">
+		<div class="text-container">
 		<h2>Lorem Ipsum</h2>
-		<p>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non urna at libero sodales blandit. 
-			Suspendisse sed mi eu sem blandit tristique. Sed eleifend nisl non lacus vulputate, eget posuere massa 
-			hendrerit. Proin auctor, elit eget consectetur tristique, dolor risus malesuada nisi, ac interdum quam 
-			libero a felis. Sed a tincidunt arcu. Nullam tincidunt semper orci, at lobortis dui congue nec. Curabitur 
-			tincidunt, nisi nec rhoncus feugiat, erat lorem iaculis metus, id venenatis nunc lectus sed nisl. Quisque 
-			ut euismod nulla. Sed vel lectus ipsum. Vivamus at nunc nec nisi eleifend congue. Vivamus convallis sapien 
-			ut dapibus tincidunt. Vivamus sit amet ipsum eget justo facilisis venenatis. Nulla facilisi.
-		</p>
-		
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non urna at libero sodales blandit. 
+				Suspendisse sed mi eu sem blandit tristique. Sed eleifend nisl non lacus vulputate, eget posuere massa 
+				hendrerit. Proin auctor, elit eget consectetur tristique, dolor risus malesuada nisi, ac interdum quam 
+				libero a felis. Sed a tincidunt arcu. Nullam tincidunt semper orci, at lobortis dui congue nec. Curabitur 
+				tincidunt, nisi nec rhoncus feugiat, erat lorem iaculis metus, id venenatis nunc lectus sed nisl. Quisque 
+				ut euismod nulla. Sed vel lectus ipsum. Vivamus at nunc nec nisi eleifend congue. Vivamus convallis sapien 
+				ut dapibus tincidunt. Vivamus sit amet ipsum eget justo facilisis venenatis. Nulla facilisi.
+			</p>	
+		</div>
+		<div class="image-container">
+			<img class ="headshot" src="GTfall2021-1599.png" alt="headshot" height="300px" width="200px">
+		</div>	
 	</div>
 </main>
 
@@ -27,4 +31,5 @@
 	.big-paragraph {
 		text-align: center;
 	}
+	
 </style>
