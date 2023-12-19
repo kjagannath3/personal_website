8255e4d8ad2d7e48ff88250f640cb9817b595da6
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-19 16:54:57 +0530

    Created base content for projects

--- svelte-app/src/Projects.svelte
+++ svelte-app/src/Projects.svelte
@@ -5,20 +5,17 @@
 
 <Navbar />
 <p class="content-title">
-    Lorem Ipsum
+    My projects
 </p>
 <div class="content">
     <div class ="text-picture-container">
         <div class="text-container">
-        <h2>Lorem Ipsum</h2>
+        <h2>Check out some of my work below!</h2>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non urna at libero sodales blandit. 
-                Suspendisse sed mi eu sem blandit tristique. Sed eleifend nisl non lacus vulputate, eget posuere massa 
-                hendrerit. Proin auctor, elit eget consectetur tristique, dolor risus malesuada nisi, ac interdum quam 
-                libero a felis. Sed a tincidunt arcu. Nullam tincidunt semper orci, at lobortis dui congue nec. Curabitur 
-                tincidunt, nisi nec rhoncus feugiat, erat lorem iaculis metus, id venenatis nunc lectus sed nisl. Quisque 
-                ut euismod nulla. Sed vel lectus ipsum. Vivamus at nunc nec nisi eleifend congue. Vivamus convallis sapien 
-                ut dapibus tincidunt. Vivamus sit amet ipsum eget justo facilisis venenatis. Nulla facilisi.
+                The projects you see below are either from my courses at Georgia Tech, or from my personal 
+                time. Unfortunately, for the vast majority of my class-based projects, I am unable to show my 
+                entire source code due to academic integrity policies. For the code I am allowed to share, I have 
+                included the corresponding github repository next to the description. Have fun!
             </p>	
         </div>
     </div>
