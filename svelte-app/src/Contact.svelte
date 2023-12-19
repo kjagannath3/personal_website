bc24e487928eef6df2783fea1037beb90bc00946
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-19 17:31:36 +0530

    Added base content for Contacts.svelte

--- svelte-app/src/Contact.svelte
+++ svelte-app/src/Contact.svelte
@@ -3,20 +3,14 @@
 </script>
 
 <Navbar />
-<p class="content-title">Lorem Ipsum</p>
+<p class="content-title">Contacts</p>
 
 <div class="content">
     <div class ="text-picture-container">
         <div class="text-container">
-        <h2>Lorem Ipsum</h2>
+        <h2>Check out some ways to reach out! I am currently looking for internship roles in the Summer and Fall of 2024.</h2>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam non urna at libero sodales blandit. 
-                Suspendisse sed mi eu sem blandit tristique. Sed eleifend nisl non lacus vulputate, eget posuere massa 
-                hendrerit. Proin auctor, elit eget consectetur tristique, dolor risus malesuada nisi, ac interdum quam 
-                libero a felis. Sed a tincidunt arcu. Nullam tincidunt semper orci, at lobortis dui congue nec. Curabitur 
-                tincidunt, nisi nec rhoncus feugiat, erat lorem iaculis metus, id venenatis nunc lectus sed nisl. Quisque 
-                ut euismod nulla. Sed vel lectus ipsum. Vivamus at nunc nec nisi eleifend congue. Vivamus convallis sapien 
-                ut dapibus tincidunt. Vivamus sit amet ipsum eget justo facilisis venenatis. Nulla facilisi.
+                Lorem Ipsum
             </p>	
         </div>
     </div>
