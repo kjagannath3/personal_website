7d3f8d5014a20622b77e37b3f3c24b85f486e523
kjagannath3 <91837535+kjagannath3@users.noreply.github.com>
2023-12-22 19:25:10 +0530

    Got rid of some excess styling

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -24,32 +24,3 @@
 	<Route path="/lifting" component={Lifting} />
 </Router>
 
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	.big-title {
-		font-size: 7em;
-	}
-
-	.big-paragraph {
-		font-size: 1.5em;
-	}
-
-	h1 {
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
